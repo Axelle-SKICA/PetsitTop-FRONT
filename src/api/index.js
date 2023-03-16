0f87f9b58f3bff6c6aa18efc5089c4b5fbb5afe2
Axelle-SKICA <axelle.skica@gmail.com>
2023-03-16 10:49:07 +0100

    new api baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://petsittop-api.up.railway.app/',
+  baseURL: 'https://petsit-top-api.up.railway.app',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
   },
